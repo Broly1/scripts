b802ef82fd840ab4ae78a169ff3fb293c44cbe68
broly <jpegrande@gmail.com>
2023-05-11 11:31:00 -0400

    add ascii to winbatch and flash stocck firmware before pe

--- lin_flashall.sh
+++ lin_flashall.sh
@@ -21,26 +21,96 @@ _  / / / / / /_/ / /_ / /_/ /
 EOF
 }
 flashall(){
+	./fastboot getvar max-sparse-size 
 	logo
-	fastboot flash dtbo dtbo.img
+	./fastboot oem fb_mode_set 
 	logo
-	fastboot flash vendor_boot vendor_boot.img
+	./fastboot flash --slot=all partition gpt.bin 
 	logo
-	fastboot flash boot boot.img
+	./fastboot flash --slot=all bootloader bootloader.img 
 	logo
-	fastboot reboot fastboot
+	./fastboot flash --slot=all vbmeta vbmeta.img 
 	logo
-	fastboot flash system system.img
+	./fastboot flash --slot=all vbmeta_system vbmeta_system.img 
 	logo
-	fastboot flash system_ext system_ext.img
+	./fastboot flash --slot=all radio radio.img 
 	logo
-	fastboot flash product product.img
+	./fastboot flash --slot=all bluetooth BTFM.bin 
 	logo
-	fastboot flash vbmeta vbmeta.img
+	./fastboot flash --slot=all dsp dspso.bin 
 	logo
-	fastboot flash vbmeta_system vbmeta_system.img
+	./fastboot flash --slot=all spunvm spunvm.bin 
 	logo
-	fastboot flash vendor vendor.img
+	./fastboot flash --slot=all logo logo.bin 
+	logo
+	./fastboot flash --slot=all boot boot.img 
+	logo
+	./fastboot flash --slot=all vendor_boot vendor_boot.img 
+	logo
+	./fastboot flash --slot=all dtbo dtbo.img 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.0 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.1 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.2 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.3 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.4 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.5 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.6 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.7 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.8 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.9 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.10 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.11 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.12 
+	logo
+	./fastboot flash --slot=all super super.img_sparsechunk.13 
+	logoflash pe/
+	./fastboot erase carrier 
+	logo
+	./fastboot erase userdata 
+	logo
+	./fastboot erase metadata 
+	logo
+	./fastboot erase ddr 
+	logo
+	./fastboot flash --slot=all bluetooth BTFM.bin 
+	logo
+	./fastboot oem fb_mode_clear
+	./fastboot reboot bootloader 
+	logo
+	printf "\n"
+	printf "flashing PixelExperience Rom"
+	./fastboot flash dtbo pe-dtbo.img
+	logo
+	./fastboot flash vendor_boot pe-vendor_boot.img
+	logo
+	./fastboot flash boot pe-boot.img
+	logo
+	./fastboot reboot fastboot
+	logo
+	./fastboot flash system system.img
+	logo
+	./fastboot flash system_ext system_ext.img
+	logo
+	./fastboot flash product product.img
+	logo
+	./fastboot flash vbmeta pe-vbmeta.img
+	logo
+	./fastboot flash vbmeta_system pe-vbmeta_system.img
+	logo
+	./fastboot flash vendor vendor.img
 	logo
 	printf "Factory reset if you want clean install!"
 
--- win_flashall.bat
+++ win_flashall.bat
@@ -1,22 +1,74 @@
-@echo off
-title PE flasher 
-
-ECHO PixelExperience installer by Broly
-
-fastboot flash dtbo dtbo.img
-fastboot flash vendor_boot vendor_boot.img
-fastboot flash boot boot.img
+echo off 
+echo    ______      ___________      
+echo    ___  /_________  /__  /_____ 
+echo    __  __ \  _ \_  /__  /_  __ \
+echo    _  / / /  __/  / _  / / /_/ /
+echo    /_/ /_/\___//_/  /_/  \____/ 
+echo.                                 
+echo                     _____       
+echo    _______ ___________  /______ 
+echo    __  __ `__ \  __ \  __/  __ \
+echo    _  / / / / / /_/ / /_ / /_/ /
+echo    /_/ /_/ /_/\____/\__/ \____/ 
+echo.
+echo @Broly_1
+echo.
+CHOICE /C YN /M "Attention this script will erase all your data do you wish to continue?"
+IF %ERRORLEVEL% EQU 2 exit
+fastboot getvar max-sparse-size 
+fastboot oem fb_mode_set 
+fastboot flash --slot=all partition gpt.bin 
+fastboot flash --slot=all bootloader bootloader.img 
+fastboot flash --slot=all vbmeta vbmeta.img 
+fastboot flash --slot=all vbmeta_system vbmeta_system.img 
+fastboot flash --slot=all radio radio.img 
+fastboot flash --slot=all bluetooth BTFM.bin 
+fastboot flash --slot=all dsp dspso.bin 
+fastboot flash --slot=all spunvm spunvm.bin 
+fastboot flash --slot=all logo logo.bin 
+fastboot flash --slot=all boot boot.img 
+fastboot flash --slot=all vendor_boot vendor_boot.img 
+fastboot flash --slot=all dtbo dtbo.img 
+fastboot flash --slot=all super super.img_sparsechunk.0 
+fastboot flash --slot=all super super.img_sparsechunk.1 
+fastboot flash --slot=all super super.img_sparsechunk.2 
+fastboot flash --slot=all super super.img_sparsechunk.3 
+fastboot flash --slot=all super super.img_sparsechunk.4 
+fastboot flash --slot=all super super.img_sparsechunk.5 
+fastboot flash --slot=all super super.img_sparsechunk.6 
+fastboot flash --slot=all super super.img_sparsechunk.7 
+fastboot flash --slot=all super super.img_sparsechunk.8 
+fastboot flash --slot=all super super.img_sparsechunk.9 
+fastboot flash --slot=all super super.img_sparsechunk.10 
+fastboot flash --slot=all super super.img_sparsechunk.11 
+fastboot flash --slot=all super super.img_sparsechunk.12 
+fastboot flash --slot=all super super.img_sparsechunk.13 
+fastboot erase carrier 
+fastboot erase userdata 
+fastboot erase metadata 
+fastboot erase ddr 
+fastboot flash --slot=all bluetooth BTFM.bin 
+fastboot oem fb_mode_clear
+fastboot reboot bootloader 
+echo.
+echo flashing PixelExperience Rom
+echo. 
+fastboot flash dtbo pe-dtbo.img
+fastboot flash vendor_boot pe-vendor_boot.img
+fastboot flash boot pe-boot.img
 fastboot reboot fastboot
+fastboot flash dtbo pe-dtbo.img
+fastboot flash vendor_boot pe-vendor_boot.img
+fastboot flash boot pe-boot.img
 fastboot flash system system.img
 fastboot flash system_ext system_ext.img
 fastboot flash product product.img
-fastboot flash vbmeta vbmeta.img
-fastboot flash vbmeta_system vbmeta_system.img
+fastboot flash vbmeta pe-vbmeta.img
+fastboot flash vbmeta_system pe-vbmeta_system.img
 fastboot flash vendor vendor.img
-CHOICE /C YN /M "Do you whant to wipe data?"
-IF %ERRORLEVEL% EQU 1 fastboot -w
-fastboot reboot
-IF %ERRORLEVEL% EQU 2 
-ECHO Dirty flashing doesn't always work factory reset if you get stuck, trying to boot system...
-fastboot reboot
-pause
+echo PixelExperience installer by Broly
+echo.
+echo ------------------------------------------ 
+echo Flashing done roll up and check for errors
+echo ------------------------------------------ 
+pause 
